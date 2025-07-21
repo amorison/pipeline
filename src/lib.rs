94c8ee13de3b3ca1ea9b2f6e1758949686882c4d
Adrien Morison <adrien.morison@glasgow.ac.uk>
2025-07-21 15:06:47 +0100

    extract FileSpec

--- src/lib.rs
+++ src/lib.rs
@@ -2,35 +2,29 @@ use serde::{Deserialize, Serialize};
 use std::path::{Path, PathBuf};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct NewFileToProcess {
+pub struct FileSpec {
     remote_path: PathBuf,
     sha256_digest: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct NewFileToProcess(FileSpec);
+
 impl NewFileToProcess {
     pub fn new<P: AsRef<Path>>(path: P) -> Self {
-        NewFileToProcess {
+        NewFileToProcess(FileSpec {
             remote_path: PathBuf::from(path.as_ref()),
             sha256_digest: "000".to_owned(),
-        }
+        })
     }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Received {
-    remote_path: PathBuf,
-    sha256_digest: String,
-}
+pub struct Received(FileSpec);
 
 impl From<NewFileToProcess> for Received {
     fn from(value: NewFileToProcess) -> Self {
-        let NewFileToProcess {
-            remote_path,
-            sha256_digest,
-        } = value;
-        Received {
-            remote_path,
-            sha256_digest,
-        }
+        let NewFileToProcess(spec) = value;
+        Received(spec)
     }
 }
