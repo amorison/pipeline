fff2997a1721216764245737170cdd5095c568ae
Adrien Morison <adrien.morison@glasgow.ac.uk>
2025-07-21 20:14:31 +0100

    give client a more useful structure

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,22 +1,28 @@
+use std::time::Duration;
+
 use futures_util::TryStreamExt;
 use futures_util::sink::SinkExt;
-use pipeline::{NewFileToProcess, Received};
+use pipeline::{NewFileToProcess, ReadFramedJson, Received};
 use tokio::net::TcpStream;
+
+async fn listen_to_server(mut from_server: ReadFramedJson<Received>) {
+    while let Some(msg) = from_server.try_next().await.unwrap() {
+        println!("Client got: {msg:?}");
+    }
+}
 
 #[tokio::main]
 async fn main() {
     let stream = TcpStream::connect("127.0.0.1:12345").await.unwrap();
 
-    let (mut from_server, mut to_server) =
+    let (from_server, mut to_server) =
         pipeline::framed_json_channel::<Received, NewFileToProcess>(stream);
 
-    let nfp1 = NewFileToProcess::new("/home/dummy/data.txt");
-    let nfp2 = NewFileToProcess::new("/home/dummy/data3.txt");
+    tokio::spawn(listen_to_server(from_server));
 
-    to_server.send(nfp1).await.unwrap();
-    to_server.send(nfp2).await.unwrap();
-
-    while let Some(msg) = from_server.try_next().await.unwrap() {
-        println!("Client got: {msg:?}");
+    loop {
+        tokio::time::sleep(Duration::from_secs(5)).await;
+        let nfp = NewFileToProcess::new("/home/dummy/data.txt");
+        to_server.send(nfp).await.unwrap();
     }
 }
