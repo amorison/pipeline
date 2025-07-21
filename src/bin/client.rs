47cdd1ac4fcf679e9a74cf268bdc3021228891e8
Adrien Morison <adrien.morison@glasgow.ac.uk>
2025-07-22 00:17:56 +0100

    client: watch folder for new files

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,9 +1,14 @@
-use std::time::Duration;
+use std::{
+    collections::HashSet,
+    io,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
 use futures_util::TryStreamExt;
 use futures_util::sink::SinkExt;
 use pipeline::{NewFileToProcess, ReadFramedJson, Received};
-use tokio::net::TcpStream;
+use tokio::{fs, net::TcpStream};
 
 async fn listen_to_server(mut from_server: ReadFramedJson<Received>) {
     while let Some(msg) = from_server.try_next().await.unwrap() {
@@ -12,17 +17,27 @@ async fn listen_to_server(mut from_server: ReadFramedJson<Received>) {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> io::Result<()> {
     let stream = TcpStream::connect("127.0.0.1:12345").await.unwrap();
 
     let (from_server, mut to_server) =
         pipeline::framed_json_channel::<Received, NewFileToProcess>(stream);
 
+    let db = Arc::new(Mutex::new(HashSet::new()));
+
     tokio::spawn(listen_to_server(from_server));
 
     loop {
         tokio::time::sleep(Duration::from_secs(5)).await;
-        let nfp = NewFileToProcess::new("/home/dummy/data.txt");
-        to_server.send(nfp).await.unwrap();
+        let mut files = fs::read_dir(".").await?;
+        while let Some(entry) = files.next_entry().await? {
+            if entry.file_type().await?.is_file() {
+                let path = entry.path();
+                if db.try_lock().unwrap().insert(path.clone()) {
+                    let nfp = NewFileToProcess::new(path);
+                    to_server.send(nfp).await.unwrap();
+                }
+            }
+        }
     }
 }
