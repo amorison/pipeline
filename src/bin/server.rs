84547db02460d14054b2e7be751c4114f466f2bc
Adrien Morison <adrien.morison@glasgow.ac.uk>
2025-07-23 13:16:30 +0100

    server: bubble up `io::Result` instead of unwrapping

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{io, sync::Arc};
 
 use futures_util::TryStreamExt;
 use pipeline::{NewFileToProcess, Receipt};
@@ -7,25 +7,26 @@ use tokio::{
     sync::Mutex,
 };
 
-async fn handle_client(stream: TcpStream) {
+async fn handle_client(stream: TcpStream) -> io::Result<()> {
     let (mut from_client, to_client) =
         pipeline::framed_json_channel::<NewFileToProcess, Receipt>(stream);
     let to_client = Arc::new(Mutex::new(to_client));
 
-    while let Some(msg) = from_client.try_next().await.unwrap() {
+    while let Some(msg) = from_client.try_next().await? {
         println!("Server got: {msg:?}");
         tokio::spawn(pipeline::processing_pipeline(msg, to_client.clone()));
     }
+    Ok(())
 }
 
 #[tokio::main]
-async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:12345").await.unwrap();
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:12345").await?;
 
     println!("Server listening on {:?}", listener.local_addr());
 
     loop {
-        let (socket, addr) = listener.accept().await.unwrap();
+        let (socket, addr) = listener.accept().await?;
         println!("Server got connection request from {addr:?}");
         tokio::spawn(handle_client(socket));
     }
