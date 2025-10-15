1359823a3de7f958046fc72e2e65c480e011f120
Adrien Morison <adrien.morison@glasgow.ac.uk>
2025-10-16 00:14:37 +0100

    perf: spread `create-buckets` over 256 tokio tasks

--- src/server/create_buckets.rs
+++ src/server/create_buckets.rs
@@ -1,15 +1,29 @@
-use std::fs;
+use std::sync::Arc;
 
-use tokio::io;
+use tokio::{fs, io};
 
 use crate::server::Config;
 
 pub(crate) async fn main(config: Config) -> io::Result<()> {
+    let config = Arc::new(config);
+    let mut handles = Vec::with_capacity(256);
+
     for i in 0..256 {
-        for j in 0..256 {
-            let bucket = config.incoming_path(format!("{i:02x}/{j:02x}"));
-            fs::create_dir_all(bucket)?;
-        }
+        let conf = config.clone();
+        let handle = tokio::spawn(async move {
+            for j in 0..256 {
+                let bucket = conf.incoming_path(format!("{i:02x}/{j:02x}"));
+                fs::create_dir_all(bucket)
+                    .await
+                    .expect("cannot create {bucket}");
+            }
+        });
+        handles.push(handle);
     }
+
+    for handle in handles {
+        handle.await?;
+    }
+
     Ok(())
 }
