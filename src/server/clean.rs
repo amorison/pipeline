c5839028f8ccda628ab7aa093b816abca341ace2
Adrien Morison <adrien.morison@gmail.com>
2026-02-02 20:14:46 +0000

    clean: print number of removed files and total size

--- src/server/clean.rs
+++ src/server/clean.rs
@@ -1,4 +1,4 @@
-use std::{io, sync::Arc};
+use std::{fmt::Display, fs::Metadata, io, sync::Arc};
 
 use log::{debug, warn};
 
@@ -10,18 +10,69 @@ use crate::{
     },
 };
 
+fn format_size(size: u64) -> String {
+    const GIBI: u64 = 1024u64.pow(3);
+    const MEBI: u64 = 1024u64.pow(2);
+    const KIBI: u64 = 1024u64.pow(1);
+    let (size, unit) = if size > GIBI {
+        (size as f64 / GIBI as f64, "GiB")
+    } else if size > MEBI {
+        (size as f64 / MEBI as f64, "MiB")
+    } else if size > KIBI {
+        (size as f64 / KIBI as f64, "kiB")
+    } else {
+        (size as f64, "B")
+    };
+    if unit == "B" {
+        format!("{size:.0} {unit}")
+    } else {
+        format!("{size:.1} {unit}")
+    }
+}
+
+pub(super) struct CleanSummary {
+    nfiles: u32,
+    total_size: u64,
+}
+
+impl CleanSummary {
+    fn new() -> CleanSummary {
+        CleanSummary {
+            nfiles: 0,
+            total_size: 0,
+        }
+    }
+
+    fn add(&mut self, meta: Metadata) {
+        self.nfiles += 1;
+        self.total_size += meta.len();
+    }
+}
+
+impl Display for CleanSummary {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let fmt_size = format_size(self.total_size);
+        write!(f, "deleted {} files ({fmt_size})", self.nfiles)
+    }
+}
+
 pub(super) async fn clean_tasks_with_status(
     config: Arc<Config>,
     db: Database,
     status: ProcessStatus,
-) {
+) -> CleanSummary {
     debug!("looking for tasks to prune");
+    let mut summary = CleanSummary::new();
     let to_prune = db.tasks_with_status(status).await;
     match to_prune {
         Ok(to_prune) => {
             for spec in to_prune.into_iter().map(FileSpec::from) {
                 debug!("pruning {spec:?}");
                 let server_path = config.path_of(&spec);
+                match tokio::fs::metadata(&server_path).await {
+                    Ok(meta) => summary.add(meta),
+                    Err(err) => warn!("error gathering metadata for {spec:?}: {err}"),
+                }
                 if let Err(err) = tokio::fs::remove_file(&server_path).await {
                     warn!("error pruning {spec:?}: {err}")
                 }
@@ -32,6 +83,7 @@ pub(super) async fn clean_tasks_with_status(
         }
         Err(err) => warn!("error when querying db: {err}"),
     }
+    summary
 }
 
 pub(crate) async fn main(config: Config, include_done: bool) -> io::Result<()> {
@@ -42,10 +94,13 @@ pub(crate) async fn main(config: Config, include_done: bool) -> io::Result<()> {
     let config = Arc::new(config);
 
     if include_done {
-        clean_tasks_with_status(config.clone(), db.clone(), ProcessStatus::Done).await;
+        let summary =
+            clean_tasks_with_status(config.clone(), db.clone(), ProcessStatus::Done).await;
+        println!("Done files: {summary}")
     }
 
-    clean_tasks_with_status(config, db, ProcessStatus::ToPrune).await;
+    let summary = clean_tasks_with_status(config, db, ProcessStatus::ToPrune).await;
+    println!("ToPrune files: {summary}");
 
     Ok(())
 }
