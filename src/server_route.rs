f0da2c57f2642920f6981e5e610329c5b6705c70
Adrien Morison <adrien.morison@gmail.com>
2026-06-28 17:22:17 +0100

    test that configs are valid `QueryConfig`s

--- src/server_route.rs
+++ src/server_route.rs
@@ -213,3 +213,26 @@ impl ServerRoute {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn default_server_conf_as_query() {
+        let bytes = crate::server::DEFAULT_TOML_CONF.as_bytes();
+        assert!(toml::from_slice::<QueryConfig>(bytes).is_ok())
+    }
+
+    #[test]
+    fn default_client_conf_as_query() {
+        let bytes = crate::client::DEFAULT_TOML_CONF.as_bytes();
+        assert!(toml::from_slice::<QueryConfig>(bytes).is_ok())
+    }
+
+    #[test]
+    fn tunnel_client_conf_as_query() {
+        let bytes = crate::client::TUNNEL_TOML_CONF.as_bytes();
+        assert!(toml::from_slice::<QueryConfig>(bytes).is_ok())
+    }
+}
